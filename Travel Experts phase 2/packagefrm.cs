edce576e6f7028d2c6392bdbe9d3f53d42186d96
Siddharth Nahar <siddharth.nahar04@gmail.com>
2024-11-21 12:15:48 -0700

    Package Add Feature

--- Travel Experts phase 2/packagefrm.cs
+++ Travel Experts phase 2/packagefrm.cs
@@ -5,12 +5,38 @@ namespace travel_experts_phase_2
 {
     public partial class packagefrm : Form
     {
+        PackageViewModel selectedPackage = null;
         public packagefrm()
         {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
+        {
+            PackageController packageController = new PackageController();
+            List<PackageViewModel> packages = packageController.GetAllPackages();
+            dgvPackages.DataSource = packages;
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            addOrUpdatePackageFrm addOrUpdatePackageFrm = new();
+            DialogResult result = addOrUpdatePackageFrm.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                PackageController packageController = new PackageController();
+                selectedPackage = addOrUpdatePackageFrm.Package;
+                packageController.AddPackage(selectedPackage);
+                displayAllPackages();
+            }
+        }
+
+        private void displayAllPackages()
         {
             PackageController packageController = new PackageController();
             List<PackageViewModel> packages = packageController.GetAllPackages();
