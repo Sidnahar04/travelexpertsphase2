5268a62d560e9cb7885d195b263216d02ede4620
jjjjjking <jonbradsmith@gmail.com>
2024-11-27 09:22:03 -0700

    assurance for agents branch up-to-date

--- Travel Experts phase 2/obj/Debug/net6.0-windows/travel experts phase 2.AssemblyInfo.cs
+++ Travel Experts phase 2/obj/Debug/net6.0-windows/travel experts phase 2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("travel experts phase 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d077f039f5b50d486a163e7332dbe62b0e0939cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f001eb2c70dfedd47c3c1dd8b279a8c6317e5926")]
 [assembly: System.Reflection.AssemblyProductAttribute("travel experts phase 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("travel experts phase 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
