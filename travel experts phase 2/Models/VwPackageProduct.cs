40202c38bc34370cab33b55b8fd40e2ff74cbbdb
Siddharth Nahar <siddharth.nahar04@gmail.com>
2024-11-28 12:04:21 -0700

    Add Model

---
+++ travel experts phase 2/Models/VwPackageProduct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace travel_experts_phase_2.Models
+{
+    [Keyless]
+    public partial class VwPackageProduct
+    {
+        public int PackageId { get; set; }
+        [StringLength(50)]
+        public string PkgName { get; set; } = null!;
+        [Column(TypeName = "datetime")]
+        public DateTime? PkgStartDate { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime? PkgEndDate { get; set; }
+        [StringLength(50)]
+        public string? PkgDesc { get; set; }
+        [Column(TypeName = "money")]
+        public decimal PkgBasePrice { get; set; }
+        [Column(TypeName = "money")]
+        public decimal? PkgAgencyCommission { get; set; }
+        [StringLength(50)]
+        public string? ProdName { get; set; }
+        [Column("ProductPackageID")]
+        public int? ProductPackageId { get; set; }
+        public int? ProductId { get; set; }
+    }
+}
